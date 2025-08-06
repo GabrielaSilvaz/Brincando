4b66d6ddbc23de264e35f425322366436f41d63a
Gabriela Silva <gabrielasilvaz164@gmail.com>
2025-08-06 16:39:44 -0300

    fiz com o chat gpt :/
    mini calculo automático

---
+++ Teste_js/function.js
@@ -0,0 +1,10 @@
+function somar() {
+
+    const v1 = parseFloat(document.getElementById ("valor1").value);
+
+    const v2 = parseFloat(document.getElementById ("valor2").value);
+
+    const soma = v1 + v2;
+
+    document.getElementById("resultado").innerText = "resultado " + soma;
+}
